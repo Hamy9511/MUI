e9b3f326108d7ceac6af0c0b43614d3e6338beb4
Hamilton Durán <hamy.duran@gmail.com>
2023-06-11 10:56:22 -0600

    Botones y envio de formulario

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import Typography from '@mui/material/Typography';
 
 
 function App() {
+
+  const handleSubmit = (valores) => {
+    console.log(valores)
+  }
+
   return (
     <>
     <Container component="section" maxWidth="sm">
     <Typography variant="h3" align='center' component="h1">Formulario Registro</Typography>  
-    <FormSignUp/>
+    <FormSignUp handleSubmit={handleSubmit}/>
     </Container>
     </>
   );
--- src/Components/FormSingUp.jsx
+++ src/Components/FormSingUp.jsx
@@ -3,22 +3,82 @@ import TextField from '@mui/material/TextField';
 import Switch from '@mui/material/Switch';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import { useState } from 'react';
 
 
-function FormSignUp() {
-    return <form>
+function FormSignUp({handleSubmit}) {
+    const [name,setName] = useState("@gmail.com");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [prom, setProm] = useState(true);
+    const [nov, setNov] = useState(true);
+    
+    return (
+    <form 
+    onSubmit={(e)=> 
+    {e.preventDefault() 
+    handleSubmit(nov)}}
+    >
+    
+    <TextField 
+    id="name" 
+    label="Nombre" 
+    variant="outlined" 
+    fullWidth 
+    margin='normal' 
+    onChange={(e) => {console.log(e.target.value)
+        setName(e.target.value)}}
+        value={name}
+    />
 
-    <TextField id="name" label="Nombre" variant="outlined" fullWidth margin='normal'/>
-    <TextField id="apellido" label="Apeliido" variant="outlined" fullWidth margin='normal'/>
-    <TextField id="email" label="Email" variant="outlined" fullWidth margin='normal'/>
+    <TextField 
+    id="apellido" 
+    label="Apeliido" 
+    variant="outlined" 
+    fullWidth 
+    margin='normal'
+    onChange={(e) => {console.log(e.target.value)
+        setLastName(e.target.value)}}
+        value={lastName}
+    />
+    
+    <TextField 
+    id="email" 
+    label="Email" 
+    variant="outlined" 
+    fullWidth 
+    margin='normal'
+    onChange={(e) => {console.log(e.target.value)
+        setEmail(e.target.value)}}
+        value={email}
+    />
     
     <FormGroup>
-        <FormControlLabel control={<Switch defaultChecked/>} label="Promociones"/>
+
+        <FormControlLabel 
+        control={
+        <Switch/>} 
+        label="Promociones"
+        checked={prom}
+        onChange={(e) => setProm(e.target.checked)}
+        />
+
+        <FormControlLabel 
+        control={
+        <Switch/>} 
+        label="Novedades"
+        checked={nov}
+        onChange={(e) => setNov(e.target.checked)}
+    />
+
     </FormGroup>
-    <FormControlLabel control={<Switch defaultChecked/>} label="Novedades"/>
     
-    <Button variant="contained">Registrarse</Button>
+    <Button 
+    variant="contained"
+    type='submit'>
+    Registrarse
+    </Button>
     </form>
-}
+)}
 
 export default FormSignUp;
